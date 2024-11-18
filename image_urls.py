5b39b5b3920c3fa5b531e4b2d20ff094ed9a70f3
Mugabi Trevor .L <146214824+mugabi91@users.noreply.github.com>
2024-11-18 23:39:23 +0300

    Update image_urls.py

--- image_urls.py
+++ image_urls.py
@@ -1,9 +1,9 @@
-# DATASETS 
-starwars_data_location = r".\Datasets\star_wars.csv"
+# DATASETS location
+starwars_data_location = r"./Datasets/star_wars.csv"
 
 # image vars
-vaders_starwars_pic =r".\images\vader.jpg"
+vaders_starwars_pic =r"./images/vader.jpg"
 caption_vaders_pic = "Darth Vader illustration"
 
-startrek_pic =r".\images\startrek.jpg"
+startrek_pic =r"./images/startrek.jpg"
 caption_startrek_pic = "Star Trek Enterprise illustration"
