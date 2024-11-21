dad69a787f677acd8d8bf18b5bf33c24d54be1f3
Mugabi T <trevortravis91@gmail.com>
2024-11-21 18:16:50 +0300

    sider

--- css.py
+++ css.py
@@ -32,7 +32,7 @@ css_sheet ="""
         background-color:#041527;
         position: sticky;
         top: 0;  /* Keeps it at the top when you scroll */
-        width: 400px;
+        width: 350px;
         height: 100vh;  /* Make the sidebar take full height of the viewport */
         padding: 20px;
         color: white;
