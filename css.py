dd20ad0ca4676bf1fb7eab64e436790f07dd89f8
Mugabi Trevor .L <146214824+mugabi91@users.noreply.github.com>
2025-07-05 16:29:47 +0300

    Update css.py

--- css.py
+++ css.py
@@ -91,8 +91,8 @@ css_sheet ="""
     
     /* Profile image styling (circular cut-out and centered) */
     .sidebar-profile-img {
-        width: 70px;
-        border-radius: 60%;
+        width: 200px;
+        /*border-radius: 60%;*/
         border: 3px dashed #4577bc;  /* Optional border around the image */
         margin-bottom: 20px;
         
